208ef6dc2e28fd5ecaf4e393ab01879ebb475b8f
BernardPouw <bernardqlui@gmail.com>
2022-10-07 11:51:23 +0700

    added packages

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,15 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Hey!</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          BIGGEST <code>BIGGEST</code>
+        <Header title="Hey there!" />
+        <p className="description" class="animate__animated animate__bounce">
+          I have something that I want to <code>TELL YOU!</code>
         </p>
       </main>
 
